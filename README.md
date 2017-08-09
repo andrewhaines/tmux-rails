dd8fe653a82280fcc019a24f42a94d453a8b41ad
Andrew Haines <andrew@fiatinsight.com>
2017-08-08 21:26:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# tmux-rails
+
+This script launches a Rails development environment in tmux. It takes a variable for the project name. The directory structure should be updated to match your local setup.
+
+To start up run:
+
+```console
+sh ~/tmux-rails/launch.sh [project_name]
+```
+
+This will open three windows: one for git (listing the local branches); one to tail the development log; and one for the console. It'll also launch Atom for the project folder.
---
+++ launch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Create a tmux session for a rails project
+
+# Get to the directory, open Atom, and show git branches
+dir="~/Projects/"$1
+tmux -2 new-session -d -s $1 -n 'git' 'cd '"$dir"'; atom .; git branch; bash'
+
+# Tail the dev log in a new window
+tmux new-window -n 'devlog' 'tail -f '"$dir"'/log/development.log; read'
+
+# Run the console in a new window
+tmux new-window -n 'console' 'cd '"$dir"'; rails c'
+
+# Then select the git window and attach the session
+tmux select-window -t $1:1
+tmux -2 attach-session -t $1
